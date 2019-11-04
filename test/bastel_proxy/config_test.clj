46a89b86b7792c67e0888b4df1f0b22a7476b8d1
Roman Stoffel <roman.stoffel@gamlor.info>
2019-11-04 16:08:38 +0900

    Fix tests

--- test/bastel_proxy/config_test.clj
+++ test/bastel_proxy/config_test.clj
@@ -3,6 +3,6 @@
   (:require [clojure.test :as t]))
 
 (t/deftest remove-unbalanced-port-config
-  (t/is (= defaults (merge-defaults {})))
-  (t/is (= 81 (-> (merge-defaults {:ports {:http {:port 81}}}) :ports :http :port)))
-  (t/is (= 8080 (-> (merge-defaults {:ports {:http {:port 81}}}) :ports :http :low-privilige-port))))
+  (t/is (= defaults (deep-merge defaults {})))
+  (t/is (= 81 (-> (deep-merge defaults {:ports {:http {:port 81}}}) :ports :http :port)))
+  (t/is (= 42380 (-> (deep-merge defaults {:ports {:http {:port 81}}}) :ports :http :low-privilege-port))))
